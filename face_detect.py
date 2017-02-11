c5f00e243fe97dba0391025ec2320cb371349fc2
Yoheita Onishi <onishiyoheita@gmail.com>
2017-02-11 13:39:58 +0900

    movie face detect added

---
+++ face_detect.py
@@ -0,0 +1,35 @@
+import cv2
+
+cascade_path = "~/.pyenv/versions/anaconda3-4.2.0/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml"
+
+def detectFace(image)
+    image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
+    image_gray = cv2.equalizeHist(image_gray)
+
+    cascade = cv2.CascadeClassifier(cascade_path)
+    facerect = detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=3, minSize=(50, 50))
+
+    print "face rectangle"
+    print facerect
+
+    return facerect
+
+video_path = "movie.mp4"
+cap = cv2.VideoCapture(video_path)
+framenum = 0
+faceframenum = 0
+color = (255, 255, 255)
+
+while(1):
+    framenum += 1
+    ret, image = cap.read()
+    if not ret:
+        break
+    if framenum%10==0:
+        facerect = detectFace(image)
+        if len(facerect) == 0: continue
+        for rect in facerect:
+            croped = image[rect[1]]:rect[1]+rect[3],rect[0]:rect[0]+rect[2]]
+            cv2.imwrite("detect" + str(faceframenum) + ".jpg", croped)
+        faceframe += 1
+cap.release()
